6cb68692dfcc09c876dd3be41f124b9bad96af0b
Litro <hectormarir@gmail.com>
2025-04-10 10:07:13 +0200

    Changed attribute player to a struct with name and score. Now players are shorted

--- main.c
+++ main.c
@@ -1,28 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_LINE 2048
+
+struct players {
+    char name[10];
+    int score;
+};
 
 struct tournament {
     char name[100];
-    int *players;
+    struct players *players;
 };
+
+void shortPlayers(int n, struct tournament *tournament) {
+    for (int i = 0; i < n; i++) {
+        int j = 0;
+        while (strcmp(tournament[i].players[j].name, "stop") != 0) {
+            j++;
+        }
+        for (int k = 0; k < j; k++) {
+            for (int l = k + 1; l < j; l++) {
+                if (tournament[i].players[k].score < tournament[i].players[l].score) {
+                    int auxShort = tournament[i].players[k].score;
+                    tournament[i].players[k] = tournament[i].players[l];
+                    tournament[i].players[l].score = auxShort;
+                }
+            }
+        }
+    }
+}
 
 void printTournaments(FILE * torneos, int n, struct tournament *tournament) {
     for (int i = 0; i < n; i++) {
         fprintf(torneos,"El nombre del torneo %d es: ", i + 1);
         fprintf(torneos, "%s\n", tournament[i].name);
         fprintf(torneos,"Los jugadores del torneo son: ");
+        shortPlayers(n, tournament);
         int j = 0;
-        while (tournament[i].players[j] != 0) {
-            fprintf(torneos,"%d ", tournament[i].players[j]);
+        while (strcmp(tournament[i].players[j].name, "stop") != 0) {
+            fprintf(torneos,"Nombre: %s Puntuación: %d\n", tournament[i].players[j].name, tournament[i].players[j].score);
             j++;
         }
-        fprintf(torneos,"\n\n");
+        fprintf(torneos,"\n");
     }
 }
 
-int main() {
 
-    FILE * torneos = fopen("torneos.txt", "w");
+void printData(FILE * data, int n, struct tournament *tournament) {
+    for (int i = 0; i < n; i++) {
+        fprintf(data,"%s\n", tournament[i].name);
+        int j = 0;
+        while (strcmp(tournament[i].players[j].name, "stop") != 0) {
+            fprintf(data, "%s\n", tournament[i].players[j].name);
+            fprintf(data, "%d\n", tournament[i].players[j].score);
+            j++;
+        }
+        fprintf(data,"\n");
+    }
+}
+
+void readData(FILE * data, int *n, struct tournament *tournament) {
+    bool keepReading = true;
+    int readLine = 1;
+    int currentLine = 1;
+    char auxString[100];
+    int auxInt = 0;
+
+    while (keepReading) {
+        while (currentLine == readLine) {
+            fgets(auxString, 100, data);
+            strcat(tournament[n].name, auxString);
+            while (fgets(auxString, 100, data) != NULL) {
+                strcat(tournament[n].players[j].name, auxString);
+                fread(&tournament[n].players[j].score, sizeof(int), 1, data);
+            }
+
+        }
+    }
+
+
+}
+
+int main() {
 
     int selection;
     int n = 0;
@@ -33,38 +96,45 @@ int main() {
         exit(1);
     }
 
+    srand(time(NULL));
+
+    FILE * dataRead = fopen("data.txt", "r");
+
+
+
+
     printf("Quieres crear un torneo? (1 si, 0 no):\n");
     scanf("%d", &selection);
 
     if (selection == 1) {
         do {
-            tournaments[n].players = malloc(sizeof(int));
+            tournaments[n].players = malloc(sizeof(struct players));
             if (tournaments[n].players == NULL) {
                 printf("Error dando memoria a jugadores");
             }
             int i = 0;
             printf("\nIntroduce el nombre del torneo:\n");
             scanf("%s", &tournaments[n].name);
-            printf("Introduce el nombre de los participantes (0 para terminar):\n");
+            printf("Introduce el nombre de los participantes ('stop' para terminar):\n");
             do {
-                //int auxName;
-                tournaments[n].players = realloc(tournaments[n].players, (i * sizeof(int)) + sizeof(int));
+                tournaments[n].players = realloc(tournaments[n].players, (i + 1) * sizeof(struct players));
                 if (tournaments[n].players == NULL) {
-                    printf("Error\n");
-                    return 1;
+                    printf("Errorasd\n");
+                    exit(1);
                 }
-                scanf("%d", &tournaments[n].players[i]);
-                //printf("%d", tournaments[n].players[i]);
-                //tournaments[n].players[i] = auxName;
-                if (tournaments[n].players[i] != 0) {
+                scanf("%s", &tournaments[n].players[i].name);
+                tournaments[n].players[i].score = rand() % 1000;
+
+                if (strcmp("stop", tournaments[n].players[i].name) != 0) {
                     i++;
                 }
-            } while (tournaments[n].players[i] != 0);
+            } while (strcmp("stop", tournaments[n].players[i].name) != 0);
 
             n++;
 
             printf("Quieres crear otro torneo?\n");
             scanf("%d", &selection);
+            //printf("Test2 %d", selection);
             if (selection == 1) {
                 tournaments = realloc(tournaments, (n + 1) * sizeof(struct tournament));
                 if (tournaments == NULL) {
@@ -72,21 +142,31 @@ int main() {
                 }
             }
         } while (selection == 1);
-    } else return 0;
+    }
+
+    //printf("test1");
+
+    FILE * torneos = fopen("torneos.txt", "w");
 
     printTournaments(torneos, n, tournaments);
+
+    fclose(torneos);
+
+    FILE * data = fopen("data.txt", "w");
+
+    printData(data, n, tournaments);
+
+    fclose(data);
 
     for (int i = 0; i < n; i++) {
         free(tournaments[i].players);
     }
     free(tournaments);
 
-    fclose(torneos);
-
     return 0;
 }
 
 //TODO leer info de torneos y no borrarlos cada vez que se use
-//TODO crear el struct players con nombre y puntuación
+//crear el struct players con nombre y puntuación
 //TODO crear funcion que permita modificar información de torneos
-//TODO ordenar los jugadores de cada torneo por puntuación
+//ordenar los jugadores de cada torneo por puntuación
